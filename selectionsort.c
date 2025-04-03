0d5b555d4c4661acf6b1556429459a0dea2195ac
Parnika Nag <163747214+parnikanag@users.noreply.github.com>
2025-04-03 11:37:28 +0530

    Add files via upload

--- selectionsort.c
+++ selectionsort.c
@@ -1,46 +1,45 @@
-#include<stdio.h>
-void selectionsort(int a[], int n)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *a, int *b) 
 {
-    int i, j, min, temp;
-    for (i = 0; i < n - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < n; j++)
-        {
-            if (a[j] < a[min])
-            {
-                min = j;
-            }
-        }
-        temp = a[min];
-        a[min] = a[i];
-        a[i] = temp;
-    }
-}
-void  printarray(int arr[],int n)
-{
-	for (int i=0;i<n;i++)
-    
-    	printf("%d\t",arr[i]);
-    printf("\n");
-    
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-int main()
+void selectionSort(int arr[], int n) 
 {
-	int n,arr[20];
+	int i, j, min_idx;
+	for (i = 0; i < n-1; i++) 
+	{
+		min_idx = i;
+		for (j = i+1; j < n; j++) 
+		{
+			if (arr[j] < arr[min_idx])
+			min_idx = j;
+		}
+		swap(&arr[min_idx], &arr[i]);
+		}
+}
+int main() 
+{
+	int n;
 	printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    printf("Enter the elements: ");
-    for (int i=0;i<n;i++)
-    {
-    	scanf("%d",&arr[i]);
-    }
-    printf("\nOriginal Array:\n");
-    printarray(arr,n);
-    selectionsort(arr,n);
-    printf("Sorted Array:\n");
-    printarray(arr,n);
-    return 0;
-
+	scanf("%d", &n);
+	int arr[n];
+	srand(time(0)); 
+	for (int i = 0; i < n; i++) 
+	{
+		arr[i] = rand() % 10000; 
+	}
+	clock_t start, end;
+	double cpu_time_used;
+	start = clock();
+	selectionSort(arr, n);
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Time taken to sort %d elements: %f seconds\n", n, cpu_time_used);
+	return 0;
 }
